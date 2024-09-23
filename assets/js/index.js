b49dd7102faaf3d744a36ca4b422891ec841aa38
dacoder <bobbye23899@gmail.com>
2024-09-23 05:02:02 +0000

    Website

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,23 @@
-import "./func.js";
-
 document.getElementById(
     "copyright-text"
-).innerHTML = `${getYear()} &copy; Galactic Tourism`;
+).innerHTML = `&copy; ${getYear()} Galactic Tourism - All rights reserved.`;
 
+function backgroundChange() {
+    const backgroundURLS = [
+        "url('../img/bg/black.jpg')",
+        "url('../img/bg/purple.jpg')",
+        "url('../img/bg/stars.jpg')",
+        "url('../img/bg/blue.jpg')",
+        "url('../img/bg/pink.jpg')",
+    ];
+
+    const randomBackground = Math.floor(Math.random() * backgroundURLS.length);
+
+    document.body.style.backgroundImage = backgroundURLS[randomBackground];
+}
+
+function displayContent(displayMenu) {
+    const menu = document.getElementById(displayMenu);
+}
+
+backgroundChange();
